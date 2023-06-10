f46a6be720c1e924d076e5bee8ffaaa91113bbe7
rupert-smith <rupertwebdev@gmail.com>
2023-06-10 19:19:38 +0100

    Simplified

--- src/components/UI/StepNumber/StepNumber.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../../../assets/styles/css/main.css";
-import styles from "./_step-number.module.scss";
-
-function StepNumber(props) {
-  return <div className={`${styles["stepNumber"]}`}>{props.children}</div>;
-}
-
-export default StepNumber;
